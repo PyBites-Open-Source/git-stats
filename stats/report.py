2f802a92d6f4404e0e8e274c355b2025999ed586
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-05 20:21:52 +0200

    make column a bit wider

--- stats/report.py
+++ stats/report.py
@@ -16,17 +16,17 @@ def show_report(repo):
     for day, changes in sorted(
         get_number_of_changes_per_day(repo).items()
     ):
-        print(f"{day:<28} | {changes:>3}")
+        print(f"{day:<38} | {changes:>3}")
 
     print("\n* Number of commits per day and author:\n")
     commits = get_number_of_commits_per_day_and_author(repo)
     for day, commits_cnt in commits.items():
         print(day)
         for author, num_commits in commits_cnt.items():
-            print(f"- {author:<26} | {num_commits:>3}")
+            print(f"- {author:<36} | {num_commits:>3}")
 
     print("\n* Files that are most often found in commits:\n")
     for file, times_seen in get_most_changed_files(repo):
-        print(f"{file:<28} | {times_seen:>3}")
+        print(f"{file:<38} | {times_seen:>3}")
 
     print(sep)
