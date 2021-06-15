24a6c4af2276f47832c7d7fa6b22f1d09d73e3ab
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-15 10:45:49 +0200

    use https for clone, because GH actions complains: Permanently added the RSA host key for... not read from remote repository

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -8,7 +8,7 @@ from stats.utils import run_command
 @pytest.fixture(scope="module")
 def karmabot_dir(tmp_path_factory):
     karmabot_dir = tmp_path_factory.mktemp("karmabot") / "karmabot"
-    cmd = ("git clone git@github.com:pybob/karmabot.git"
+    cmd = ("git clone https://github.com/pybob/karmabot.git"
            f" {karmabot_dir} 2>&1 > /dev/null")
     run_command(cmd)
     yield karmabot_dir
